81e739bfa121ee3c53bb029e092ade0b8c06ae30
Yuzhe Song <qweasdzxc17856966954@outlook.com>
2026-08-19 15:57:15 +0800

    Correctly set compileSdk using CommonExtension in Kotlin DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,26 +1,20 @@
 import com.android.build.api.dsl.CommonExtension
 import java.io.File
-import java.util.Properties
-import java.io.FileInputStream
 
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+}
+
+// 强制所有 Android 子模块（包括插件）使用 compileSdk 36
+subprojects {
     afterEvaluate {
         if (project.plugins.hasPlugin("com.android.application") ||
             project.plugins.hasPlugin("com.android.library")) {
-            // 使用 CommonExtension 代替 BaseExtension
             val android = project.extensions.getByType(CommonExtension::class.java)
-            // 设置 compileSdkVersion（注意：某些版本是 compileSdk，我们同时设置两个）
-            android.compileSdkVersion = 36
-            // 如果 compileSdk 可用，也设置：
-            try {
-                android.compileSdk = 36
-            } catch (e: Exception) {
-                // 忽略，可能已设置
-            }
+            android.compileSdk = 36
         }
     }
 }
